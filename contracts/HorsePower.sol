9087b0791c15bb07d7d279c592281ea4bbe031bf
Matias Nuñez <Matiasne45@gmail.com>
2021-12-19 21:27:30 -0300

    Update and rename CryptoCars.sol to HorsePower.sol

--- contracts/HorsePower.sol
+++ contracts/HorsePower.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CryptoCars is ERC20, Pausable, Ownable {
-    constructor() ERC20("CryptoCars", "CCAR") {
+contract HorsePower is ERC20, Pausable, Ownable {
+    constructor() ERC20("HorsePower", "HPOW") {
         _mint(msg.sender, 100000000 * 10 ** decimals());
     }
 
